fd504bf28e0aec9be0bf3c694e80fdc471198cf5
van4es <super.ivan-2@Mail.ru>
2025-11-07 06:32:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using FixItNR.Api.Data;
 using Microsoft.OpenApi.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Слушаем порт 8080 во всех интерфейсах (для Amvera)
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -13,20 +14,22 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 
-// Простое In‑Memory хранилище для ПР3
 builder.Services.AddSingleton<ITicketRepository, InMemoryTicketRepository>();
-
 
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FixIt NR API v1");
+   
+});
 
 
-app.UseHttpsRedirection();
+
 app.MapControllers();
+
 app.Run();
+
+
